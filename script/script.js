2391d9c74a61ea1de5eabf99c1d30cc0a2311910
Yanira Alemán <yanira.ar@gmail.com>
2021-11-22 23:06:51 +0000

    canvas delimited

--- script/script.js
+++ script/script.js
@@ -1,3 +1,27 @@
-const fireman = document.getElementById('fireman')
+const farmer = document.getElementById('farmer')
+const canvas = document.getElementById('canvas')
+var farmerPos = 0
 
-fireman.style.left = '200px'
+function moveFarmerRight() {
+    if (farmerPos <= 740) { // que calcule según el ancho del farmer
+        farmerPos += 10
+        farmer.style.left = farmerPos + 'px'
+    }
+}
+
+function moveFarmerLeft() {
+    if (farmerPos >= 10) {
+        farmerPos -= 10
+        farmer.style.left = farmerPos + 'px'
+    }
+}
+
+window.addEventListener('keydown', function (event) {
+    if (event.code === 'ArrowRight') {
+        moveFarmerRight()
+    }
+    if (event.code === 'ArrowLeft') {
+        moveFarmerLeft()
+    }
+})
+
