1256cf733e9fddf9c4bf02913cc5ab570ff2b27b
Yanira Alemán <yanira.ar@gmail.com>
2021-11-26 19:48:52 +0000

    counter fontfamily changed

--- script/cow.js
+++ script/cow.js
@@ -1,10 +1,10 @@
 function Cow(x = 0) {
-  var self = this
   this.x = x
   this.y = 120
   this.height = 50
   this.width = 80
 
+  //***creamos las vacas***
   this.cow = document.createElement('div')
   this.cow.classList.add('cow')
   this.cow.style.left = `${this.x}px`
